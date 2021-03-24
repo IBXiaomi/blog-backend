f5ce9fc63c3472639b6f6faae943ab488dffa0b8
baxixiaomi <675837004@qq.com>
2021-03-24 23:03:28 +0800

    test cros config

--- src/main/java/com/jamiewang/controller/api/LoginController.java
+++ src/main/java/com/jamiewang/controller/api/LoginController.java
@@ -2,6 +2,7 @@ package com.jamiewang.controller.api;
 
 import com.jamiewang.common.LoginRequest;
 import com.jamiewang.common.admin.Response;
+import com.jamiewang.common.exception.ErrorMessage;
 import com.jamiewang.service.LoginService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/jamie")
 public class LoginController {
 
-   private final LoginService loginService;
+    private final LoginService loginService;
 
     public LoginController(LoginService loginService) {
         this.loginService = loginService;
@@ -22,5 +23,11 @@ public class LoginController {
     @ResponseBody
     public Response loginToSystem(@RequestBody LoginRequest request) {
         return loginService.loginToSystem(request);
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public Response testCROS() {
+        return new Response("200", "success", new ErrorMessage());
     }
 }
